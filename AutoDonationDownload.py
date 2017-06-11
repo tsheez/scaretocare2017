bee66a20d66ea95b7c968797c56a82fa62ce8b83
tsheez <t2shaw@ucsd.edu>
2017-06-10 17:56:06 -0700

    fixed more shitttt

--- AutoDonationDownload.py
+++ AutoDonationDownload.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime
+import glob, os
 
 if __name__ == '__main__':
     updateFrequency = 15 #seconds
@@ -11,6 +12,9 @@ if __name__ == '__main__':
     while True:
         try:
             if "My Donations" in driver.title:
+                files = glob.glob('C:\\Users\\tlsha\\Downloads\\donations*')
+                for file in files: os.remove(file)
+                print("Cleaned up Files")
                 driver.get("https://streamlabs.com/dashboard/donations/export")
                 print('downloaded latest at:', end = " ")
                 print(datetime.now().astimezone())
